901975fb1fe05a838d566b966195cb0d10d4f89b
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-04-09 21:42:22 +0800

    sleep() is not working but threads are randomized

--- main.py
+++ main.py
@@ -1,63 +1,73 @@
-from threading import Thread
-from threading import Semaphore
+from threading import *
 from time import sleep
-from random import random
 
 n = 0
 b = 0
 g = 0
 b_range = 0
 g_range = 0
-sleep_time = 0.5
+sleep_time = 1
+semaphore = Semaphore(1)
 
 
-def deduct_color(set_num, color, semaphore):
-    global b, g, sleep_time
-    with semaphore:
-        match color:
-            case "b":
-                i = 0
-                print("Blue only.")
-                value = random()
-                while (b > 0) & (i < n):
-                    i = i + 1
-                    b = b - 1
-                print("Empty fitting room blue: {} green: {} Set thread: {}\n".format(b, g, set_num))
-                sleep(value)
+def deduct_blue(set_num, thread_num):
+    global b, g
+    if b > 0:
+        b = b - 1
+        print("blue {}->{} green {} Thread-{} Set-{}: ".format(b + 1, b, g, thread_num, set_num, ))
+        sleep(sleep_time)
 
-            case "g":
-                i = 0
-                print("Green only.")
-                value = random()
-                while (g > 0) & (i < n):
-                    i = i + 1
-                    g = g - 1
-                print("Empty fitting room blue: {} green: {} Set thread: {}\n".format(b, g, set_num))
-                sleep(value)
+
+def deduct_green(set_num, thread_num):
+    global b, g
+    if g > 0:
+        g = g - 1
+        print("blue: {} green: {}->{} Thread-{} Set-{}: ".format(b, g + 1, g, thread_num, set_num,))
+        sleep(sleep_time)
+
+
+def create_blue_thread(set_num):
+    global sleep_time, semaphore, n
+    for i in range(n):
+        semaphore.acquire()
+        worker = Thread(target=deduct_blue, args=[set_num, i])
+        worker.start()
+        semaphore.release()
+    sleep(sleep_time)
+
+
+def create_green_thread(set_num):
+    global sleep_time, semaphore, n
+    for i in range(n):
+        semaphore.acquire()
+        worker = Thread(target=deduct_green, args=[set_num, i])
+        worker.start()
+        semaphore.release()
+    sleep(sleep_time)
 
 
 def create_color_threads():
     global b_range, g_range, n
 
-    threads_tuple = []
+    threads = []
     last_i = 0
     last_j = 0
     for i in range(int(b_range / n)):
-        threads_tuple.append((i, "b"))
         last_i = i
+        threads.append(Thread(target=create_blue_thread, args=[last_i]))
 
     for j in range(int(g_range / n)):
-        threads_tuple.append((last_i + (j + 1), "g"))
         last_j = last_i + (j + 1)
+        threads.append(Thread(target=create_green_thread, args=[last_j]))
 
     if b_range % n > 0:
-        threads_tuple.append((last_j + 1, "b"))
+        threads.append(Thread(target=create_blue_thread, args=[last_j + 1]))
         if g_range % n > 0:
-            threads_tuple.append((last_j + 2, "g"))
+            threads.append(Thread(target=create_green_thread, args=[last_j + 2]))
     elif g_range % n > 0:
-        threads_tuple.append((last_j + 1, "g"))
+        threads.append(Thread(target=create_green_thread, args=[last_j + 1]))
 
-    return threads_tuple
+    return threads
 
 
 def perform_transactions(slots, blue, green):
@@ -68,11 +78,11 @@ def perform_transactions(slots, blue, green):
     g = green
     g_range = green
     # starting processes
-    threads_tuple = create_color_threads()
+    threads = create_color_threads()
 
-    for i in range(len(threads_tuple)):
-        thread_tuple = threads_tuple[i]
-        worker = Thread(target=deduct_color, args=(thread_tuple[0], thread_tuple[1], Semaphore(1)))
+    for i in range(len(threads)):
+        worker = threads[i]
+        # sleep(sleep_time)
         worker.start()
 
 
@@ -81,4 +91,4 @@ if __name__ == "__main__":
     # n = int(x)
     # b = int(y)
     # g = int(z)
-    perform_transactions(6, 52, 20)
+    perform_transactions(6, 20, 50)
