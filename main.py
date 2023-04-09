a5d01fc89d6034edabf070e03c023a150d407e4a
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-04-09 19:15:49 +0800

    trying to randomize threads order

--- main.py
+++ main.py
@@ -1,62 +1,79 @@
-import multiprocessing
-import os
-from threading import *
-import time
+from threading import Thread
+from threading import Semaphore
+from time import sleep
+from random import random
 
 n = 0
 b = 0
 g = 0
-obj = Semaphore(n)
+b_range = 0
+g_range = 0
+sleep_time = 0.5
 
 
-# function to withdraw from account
-def deduct_blue():
-    global b, obj
-    i = 0
-    obj.acquire()
-    print("Blue only.")
-    while (b > 0) & (i < n):
-        i = i + 1
-        b = b - 1
-    print("Empty fitting room blue: {} green: {} pid: {}".format(b, g, os.getpid()))
-    time.sleep(4)
-    obj.release()
+def deduct_color(set_num, color, semaphore):
+    global b, g, sleep_time
+    with semaphore:
+        match color:
+            case "b":
+                i = 0
+                print("Blue only.")
+                value = random()
+                while (b > 0) & (i < n):
+                    i = i + 1
+                    b = b - 1
+                print("Empty fitting room blue: {} green: {} Set thread: {}\n".format(b, g, set_num))
+                sleep(value)
+
+            case "g":
+                i = 0
+                print("Green only.")
+                value = random()
+                while (g > 0) & (i < n):
+                    i = i + 1
+                    g = g - 1
+                print("Empty fitting room blue: {} green: {} Set thread: {}\n".format(b, g, set_num))
+                sleep(value)
 
 
-# function to deposit to account
-def deduct_green():
-    global g, obj
-    i = 0
-    obj.acquire()
-    print("Green only.")
-    while (g > 0) & (i < n):
-        i = i + 1
-        g = g - 1
-    print("Empty fitting room blue: {} green: {} pid: {}".format(b, g, os.getpid()))
-    time.sleep(4)
-    obj.release()
+def create_color_threads():
+    global b_range, g_range, n
 
+    threads_tuple = []
+    last_i = 0
+    last_j = 0
+    for i in range(int(b_range / n)):
+        threads_tuple.append((i, "b"))
+        last_i = i
 
-def create_threads():
-    # creating new processes
-    p1 = Thread(target=deduct_blue, args=())
-    p2 = Thread(target=deduct_green, args=())
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.join()
+    for j in range(int(g_range / n)):
+        threads_tuple.append((last_i + (j + 1), "g"))
+        last_j = last_i + (j + 1)
+
+    if b_range % n > 0:
+        threads_tuple.append((last_j + 1, "b"))
+        if g_range % n > 0:
+            threads_tuple.append((last_j + 2, "g"))
+    elif g_range % n > 0:
+        threads_tuple.append((last_j + 1, "g"))
+
+    return threads_tuple
 
 
 def perform_transactions(slots, blue, green):
-    global n, b, g, obj
+    global n, b, g, b_range, g_range
     n = slots
     b = blue
+    b_range = blue
     g = green
-    obj = Semaphore(n)
-
+    g_range = green
     # starting processes
-    while b + g > 0:
-        create_threads()
+    threads_tuple = create_color_threads()
+
+    for i in range(len(threads_tuple)):
+        thread_tuple = threads_tuple[i]
+        worker = Thread(target=deduct_color, args=(thread_tuple[0], thread_tuple[1], Semaphore(1)))
+        worker.start()
 
 
 if __name__ == "__main__":
@@ -64,4 +81,4 @@ if __name__ == "__main__":
     # n = int(x)
     # b = int(y)
     # g = int(z)
-    perform_transactions(6, 8, 20)
+    perform_transactions(6, 52, 20)
